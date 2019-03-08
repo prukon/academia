9c1ab6d1857d8e61df4b100a5d2d428896ff54ea
prukon <prukon@gmail.com>
2019-03-08 21:44:50 +0300

    Урок 5

---
+++ lesson 6/index.js
@@ -0,0 +1,17 @@
+'use strict';
+let btn = document.getElementsByTagName('button');
+
+btn[0].onclick = function() {
+    console.log('Вы нажали первую кнопку');
+};
+
+
+
+// btn[0].addEventListener('click',function(){
+//     alert('Вы нажали первую кнопку');
+// });
+
+// btn[0].addEventListener('click',function(){
+//     alert('Вы опять нажали на первую кнопку');
+// });
+
