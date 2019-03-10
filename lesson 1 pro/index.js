7f4fc4a58760ccabe8100ab04a233f625045a01f
prukon <prukon@gmail.com>
2019-03-10 03:05:33 +0300

    Урок 1 pro

--- lesson 1 pro/index.js
+++ lesson 1 pro/index.js
@@ -1,1 +1,9 @@
 'use strict';
+let num  = 33721; //1) Создать переменную num со значением 33721
+num = num + "";
+let result = num[0]*num[1]*num[2]*num[3]*num[4];
+
+let result2 = result**3; //2) Полученный результат возвести в степень 3, используя только 1 оператор
+let result3 = result2 +"";
+result3  = result3[0] + result3[1]; 
+console.log(result3);  // Вывести на экран первые 2 цифры полученного числа
