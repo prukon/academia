427a3729663a622adde330523f73f7330662f74e
prukon <prukon@gmail.com>
2019-03-11 23:12:23 +0300

    Урок 7

---
+++ lesson 7/js/script.js
@@ -0,0 +1,54 @@
+// window.addEventListener('DOMContentLoaded', function () {
+// 'use strict';
+//     let tab = document.querySelectorAll('.info-header-tab'),
+//         info = document.querySelector('.info-header'),
+//         tabContent = document.querySelectorAll('.info-tabcontent');
+
+//     function hideTabContent(a) {
+//         for (let i = a; i < tabContent.length; i++) {
+//             tabContent[i].classList.remove('show');
+//             tabContent[i].classList.add('hide');
+//         }
+//     }
+//     hideTabContent(1);
+
+//     function showTabContent(b) {
+//         if (tabContent[b].classList.contains('hide')) {
+//             tabContent[b].classList.remove('hide');
+//             tabContent[b].classList.add('show');
+//         }
+//     }
+//     info.addEventListener('click', function (event) {
+//         let target = event.target;
+//         if (target && target.classList.contains('info-header-tab')) {
+//             for (let i = 0; i < tab.length; i++) {
+//                 if (target == tab[i]) {
+//                     hideTabContent(0);
+//                     showTabContent(i);
+//                 }
+//             }
+//         }
+//     })
+// // });
+
+
+
+//Home Work
+
+let date = new Date('Marth 10, 2018 23:15:30');
+date = new Date();
+
+let hours = date.getHours(), //часы
+    minutes = date.getMinutes(), //минуты
+    seconds = date.getSeconds(), //секунды
+    setHours = document.querySelector('#hours');
+    setMinutes = document.querySelector('#minutes');
+    setSeconds = document.querySelector('#seconds');
+
+
+let timerLd = setInterval(sayHello, 1000);
+function sayHello(){
+    setHours.textContent  = hours;
+    setMinutes.textContent  = minutes;
+    setSeconds.textContent  = seconds;
+ }
---
+++ lesson 7/test/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// let timerLd = setTimeout(sayHello, 3000);
+// clearTimeout(timerLd); //отмена вызова 
+
+// let timerLd = setInterval(sayHello, 3000);
+// clearTimeout(timerLd);
+
+// function sayHello(){
+//     alert("Hello, World");
+// }
+
+// function sayHello(){
+// console.log("hello, world")
+//  }
+
+
+// //ресурсивный вызов setTimeout
+// let timerLd = setTimeout(function log(){
+//     console.log("hello");
+//     setTimeout(log, 2000)
+// }, 3000);
+
+let btn = document.querySelector('.btn'),
+    elem = document.querySelector('.box');
+
+function myAnimation() {
+    let pos = 0;
+    let id = setInterval(frame, 10);
+
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener.length('click', myAnimation);
+
+//дегегирование
+let btnBlock = document.querySelector('.btn-block'),
+btns = document.getElementsByTagName('button');
+btnBlock.addEventListener('click',function(event){
+if(event.target && event.target.target == 'BUTTON'){
+    if(event.target && event.target.matches('button.first')){
+
+//if(event.target && event.сlassList.contains('first')){
+
+    console.log(333);
+}
+})
