cce61a68ee5cc9b176515871947eee80e6986c8b
prukon <prukon@gmail.com>
2019-03-15 21:32:08 +0300

    Урок 8

--- lesson 8/js/script.js
+++ lesson 8/js/script.js
@@ -1,4 +1,40 @@
-let deadline = '2019-03-15';
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+        let tab = document.querySelectorAll('.info-header-tab'),
+            info = document.querySelector('.info-header'),
+            tabContent = document.querySelectorAll('.info-tabcontent');
+    
+        function hideTabContent(a) {
+            for (let i = a; i < tabContent.length; i++) {
+                tabContent[i].classList.remove('show');
+                tabContent[i].classList.add('hide');
+            }
+        }
+        hideTabContent(1);
+    
+        function showTabContent(b) {
+            if (tabContent[b].classList.contains('hide')) {
+                tabContent[b].classList.remove('hide');
+                tabContent[b].classList.add('show');
+            }
+        }
+        info.addEventListener('click', function (event) {
+            let target = event.target;
+            if (target && target.classList.contains('info-header-tab')) {
+                for (let i = 0; i < tab.length; i++) {
+                    if (target == tab[i]) {
+                        hideTabContent(0);
+                        showTabContent(i);
+                    }
+                }
+            }
+        })
+    });
+    
+
+
+
+let deadline = '2019-03-16';
 
 function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
