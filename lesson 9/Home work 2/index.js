f6f55f4bb00cbcc9a27a10709f2db7ba49246d51
prukon <prukon@gmail.com>
2019-03-16 16:56:30 +0300

    Урок 9 запятые

--- lesson 9/Home work 2/index.js
+++ lesson 9/Home work 2/index.js
@@ -1,7 +1,7 @@
 let age = document.getElementById('age');
 age.addEventListener('change', function () {
-    console.log(showUser.apply(age, ['Snow', 'John'])) 
-})
+    console.log(showUser.apply(age, ['Snow', 'John'])); 
+});
 
 function showUser(surname, name) {   
     alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
--- lesson 9/landing/js/script.js
+++ lesson 9/landing/js/script.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 
-    })
+    });
 });
 
 
@@ -54,7 +54,7 @@ function getTimeRemaining(endtime) {
         'hours': hours,
         'minutes': minutes,
         'seconds': seconds
-    }
+    };
 }
 
 function setClock(id, endtime) {
@@ -122,20 +122,20 @@ more.addEventListener('click', function () {
     overlay.style.display = 'block';
     this.classList.add('more-splash');
     document.body.style.overflow = 'hidden';
-})
+});
 
 for(let i = 0; i<descriptionBtn.length; i++){
     descriptionBtn[i].addEventListener('click', function () {
         overlay.style.display = 'block';
         this.classList.add('more-splash');
         document.body.style.overflow = 'hidden';
-    })
+    });
 }
 close.addEventListener('click', function () {
     overlay.style.display = 'none';
     more.classList.remove('more-splash');
     document.body.style.overflow = '';
 
-})
+});
 
 
