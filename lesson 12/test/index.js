d20942f1a63a156573d47da4070cc1f471e929a8
prukon <prukon@gmail.com>
2019-03-22 21:45:15 +0300

    12

--- lesson 12/test/index.js
+++ lesson 12/test/index.js
@@ -1,34 +1,24 @@
 'use strict';
 let drink = 0;
-
 // function shoot(arrow, headshot, fail){
 //     console.log("Вы сделали выстрел");
-
 //     setTimeout(function(){
 //         Math.random() > .5 ? headshot({}) : fail("Вы промахнулись");
 //     }, 1000)
 // };
-
-
-
 // function win() {
 //     console.log('Вы победили!');
 //     (drink == 1) ? buyBeer(): giveMoney();
 // }
-
 // function buyBeer() {
 //     console.log('Вам купили пиво!');
 // }
-
 // function lose() {
 //     console.log('Вы проиграли!');
 // }
-
 // function giveMoney() {
 //     console.log('Вам заплатили!');
 // }
-
-
 // shoot({},
 //     function (mark) {
 //         console.log('Вы попали в цель!');
@@ -38,8 +28,6 @@ let drink = 0;
 //         console.error(miss);
 //         lose();
 //     })
-
-    
 function shoot(arrow) {
     console.log("Вы сделали выстрел");
     let promise = new Promise(function (resolve, reject) {
@@ -48,29 +36,21 @@ function shoot(arrow) {
         }, 1000)
     });
     return promise;
-  
 };
-
 function win() {
     console.log('Вы победили!');
     (drink == 1) ? buyBeer(): giveMoney();
 }
-
 function buyBeer() {
     console.log('Вам купили пиво!');
 }
-
 function lose() {
     console.log('Вы проиграли!');
 }
-
 function giveMoney() {
     console.log('Вам заплатили!');
 }
-
-
 shoot({})
     .then(mark => console.log('Вы попали в цель!'))
     .then(win)
     .catch(lose)
-  
