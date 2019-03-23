ab0e89340429f49ab6d425363875c3ddfee3048a
prukon <prukon@gmail.com>
2019-03-23 20:33:08 +0300

    14 fix

--- lesson 14/Homework/index.js
+++ lesson 14/Homework/index.js
@@ -1,11 +1,28 @@
+let a, b;
 $(document).ready(function () {
     $('.main_btna').on('click', function () {
-        $('.overlay').fadeToggle('slow');
-        $('.modal').slideDown("slow");
+        giveModal(a = 1);
     });
-});
+    $('.main_btn').on('click', function () {
+        giveModal(a = 1);
+    });
+    $('.main_nav nav ul li:eq(1)').on('click', function () {
+        giveModal(a = 1);
+    });
+    $('.modal button span').on('click', function () {
+        giveModal(b = 1);
+    });
 
-$('.modal button span').on('click', function(){
-    $('.overlay').fadeToggle('slow');
-    $('.modal').slideUp("slow");
-})
+    
+    function giveModal() {
+        $('.overlay').fadeToggle('slow');
+        if (a == 1) {
+            $('.modal').slideDown("slow");
+            a = "";
+        }
+        if (b == 1) {
+            $('.modal').slideUp("slow");
+            b = "";
+        }
+    }
+});
