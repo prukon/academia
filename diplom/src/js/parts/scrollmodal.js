04d186c494b5f5b1ffaf235828066654887542dd
prukon <prukon@gmail.com>
2019-04-03 23:57:36 +0300

    diplom 11

---
+++ diplom/src/js/parts/scrollmodal.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function scrollmodal() {
+    let popupGift = document.querySelector('.popup-gift');
+
+
+
+
+    // if($(window).scrollTop()+$(window).height()>=$(document).height())
+
+
+
+
+}
+export default scrollmodal;
+
+
+
+// Так найди высоту body. Если pageYOffset будет равен высоте body за вычетом высоты viewport то значит доскролил до конца
